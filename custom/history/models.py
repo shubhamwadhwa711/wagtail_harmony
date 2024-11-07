05cb37eb028d3f42536005f657961479b0ef49de
shubhamwadhwa711 <shubhamwadhwa711@gmail.com>
2024-11-07 15:04:45 +0530

    history page filter pending

--- custom/history/models.py
+++ custom/history/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
+# Create your models here.
+# core/models.py
+
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel,StreamValue,InlinePanel
+
 from core.richtext.models import RichTextPageAbstract
 from blocks.richtext import richtext_blocks
 from wagtail.models import Orderable, Site
 from modelcluster.fields import ParentalKey
 from wagtail.models import Page
 from wagtail.fields import RichTextField, StreamField
-from wagtail.images.models import Image
-from wagtail.blocks import RichTextBlock
-from wagtail.models import Orderable
+from django.template.response import TemplateResponse
 
 ##################################################################################################
 
@@ -23,21 +25,74 @@ class HistoriesPage(RichTextPageAbstract):
     heading = models.TextField(blank=True, null=True,default="History of Harmony")
     short_heading = models.TextField(blank=True, null=True)
     short_description = models.TextField(blank=True, null=True)
+    bottom_heading = models.TextField(blank=True, null=True,default="Share your story about Harmony")
+    bottom_image_one = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        related_name='+',
+        blank=True,
+        null=True,
+    )
+    bottom_image_two = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        related_name='+',
+        blank=True,
+        null=True,
+    )
+    bottom_button_text = models.TextField(blank=True, null=True,default ="Share Story")
+    bottom_link_page = models.ForeignKey(
+        'wagtailcore.Page',
+        on_delete=models.SET_NULL,
+        related_name='+',
+        blank=True,
+        null=True,
+    )
+
     content_panels = RichTextPageAbstract.content_panels + [
 
         FieldPanel("heading"),
         FieldPanel("short_heading"),
         FieldPanel("short_description"),
+        MultiFieldPanel([
+            FieldPanel('bottom_heading'),
+            FieldPanel('bottom_image_one'),
+            FieldPanel('bottom_image_two'),
+            FieldPanel('bottom_button_text'),
+            FieldPanel('bottom_link_page'),
+        ], heading='Add Bottom Section'),
 
     ]
 
     parent_page_types = ['home.HomePage']
     subpage_types = ["history.HistoryPage"]
-
-
     class Meta:
         verbose_name = 'Histories Page'
         verbose_name_plural = 'Histories Pages'
+    
+    def update_context(self,context):
+        histories = HistoryPage.objects.all()
+        context.update({
+            'histories': histories,
+           
+        })
+        return context
+
+    def serve(self,request,*args, **kwargs):
+        request.is_preview = False
+        template = self.get_template(request, *args, **kwargs)
+        default_context = self.get_context(request, *args, **kwargs)
+        context = self.update_context(default_context)
+        
+        return TemplateResponse(
+            request,
+            template,
+            context,
+        )
+
+
+
+    
 
 
 
