aa9e442216bfd85fac4786626823fd1d9e601882
shubhamwadhwa711 <shubhamwadhwa711@gmail.com>
2024-11-08 10:08:49 +0530

    fix  the event page

--- custom/events/models.py
+++ custom/events/models.py
@@ -20,6 +20,9 @@ from wagtail.contrib.forms.models import (
     AbstractFormField,
 )
 
+
+
+from django.template.response import TemplateResponse
 ##################################################################################################
 
 
@@ -31,11 +34,54 @@ class EventsPage(RichTextPageAbstract):
         blank=True,
     )
     #show the latest event as an html
-
     heading = models.TextField(blank=True, null=True,default="EVENTS")
-    content_panels = RichTextPageAbstract.content_panels + [       
+    button_text = models.TextField(blank=True, null=True,default ="Event Details")
+    link_page = models.ForeignKey(
+        'wagtailcore.Page',
+        on_delete=models.SET_NULL,
+        related_name='+',
+        blank=True,
+        null=True,
+    )
+
+    bottom_heading = models.TextField(blank=True, null=True)
+    bottom_image_one = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        related_name='+',
+        blank=True,
+        null=True,
+    )
+    bottom_image_two = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        related_name='+',
+        blank=True,
+        null=True,
+    )
+    bottom_button_text = models.TextField(blank=True, null=True,default ="Contact us")
+    bottom_link_page = models.ForeignKey(
+        'wagtailcore.Page',
+        on_delete=models.SET_NULL,
+        related_name='+',
+        blank=True,
+        null=True,
+    )
+
+    content_panels = RichTextPageAbstract.content_panels + [  
+        FieldPanel("heading"),
+        FieldPanel("button_text"),     
+        FieldPanel("link_page"),
+        MultiFieldPanel([
+            FieldPanel('bottom_heading'),
+            FieldPanel('bottom_image_one'),
+            FieldPanel('bottom_image_two'),
+            FieldPanel('bottom_button_text'),
+            FieldPanel('bottom_link_page'),
+        ], heading='Add Bottom Section'),
 
     ]
+
 
     parent_page_types = ['home.HomePage']
     subpage_types = [
@@ -46,6 +92,27 @@ class EventsPage(RichTextPageAbstract):
         verbose_name = 'Events Page'
         verbose_name_plural = 'Events Pages'
 
+
+
+    def update_context(self,context):
+        events = EventPage.objects.all()
+        context.update({
+            'events': events,
+    
+        })
+        return context
+
+    def serve(self,request,*args, **kwargs):
+        request.is_preview = False
+        template = self.get_template(request, *args, **kwargs)
+        default_context = self.get_context(request, *args, **kwargs)
+        context = self.update_context(default_context)
+       
+        return TemplateResponse(
+            request,
+            template,
+            context,
+        )
 
 
 
@@ -65,12 +132,14 @@ class EventPage(RichTextPageAbstract):
         blank=True,
         null=True,
     )
-
-
-    description = models.TextField(blank=True, null=True)
     event_date = models.DateField(blank=True, null=True)
+    event_details_heading = models.TextField(blank=True, null=True)
+    event_short_description = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    
 
-    button_text = models.TextField(blank=True, null=True)
+
+    button_text = models.TextField(blank=True, null=True,default="Add to Calender")
     link_page = models.ForeignKey(
         'wagtailcore.Page',
         on_delete=models.SET_NULL,
@@ -78,10 +147,8 @@ class EventPage(RichTextPageAbstract):
         blank=True,
         null=True,
     )
-    event_short_heading = models.TextField(blank=True, null=True)
-    event_short_description = models.TextField(blank=True, null=True)
-    event_full_description = models.TextField(blank=True, null=True)
-
+    
+    # event_full_description = models.TextField(blank=True, null=True)
     button_text_two = models.TextField(blank=True, null=True)
     link_page_two= models.ForeignKey(
         'wagtailcore.Page',
@@ -91,24 +158,57 @@ class EventPage(RichTextPageAbstract):
         null=True,
     )
 
+    bottom_heading = models.TextField(blank=True, null=True)
+    bottom_image_one = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        related_name='+',
+        blank=True,
+        null=True,
+    )
+    bottom_image_two = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        related_name='+',
+        blank=True,
+        null=True,
+    )
+    bottom_button_text = models.TextField(blank=True, null=True,default ="Contact us")
+    bottom_link_page = models.ForeignKey(
+        'wagtailcore.Page',
+        on_delete=models.SET_NULL,
+        related_name='+',
+        blank=True,
+        null=True,
+    )
+
     content_panels = RichTextPageAbstract.content_panels + [
-        
+        FieldPanel("page_name"),
         FieldPanel("heading"),
         FieldPanel("event_bg_image"),
-        # FieldPanel("description"),
         FieldPanel("event_date"),
         MultiFieldPanel([
             FieldPanel('button_text'),
             FieldPanel('link_page'),
         ], heading='Add Event Page Button'),
-        FieldPanel("event_short_heading"),
+        FieldPanel("event_details_heading"),
         FieldPanel("event_short_description"),
+        
         InlinePanel('event_page_images', label='Event Pages Images'),
-        FieldPanel("event_full_description"),
+        FieldPanel("description"),
         MultiFieldPanel([
             FieldPanel('button_text_two'),
             FieldPanel('link_page_two'),
-        ], heading='Buttom Add Event Page Button'),
+        ], heading='Event Calender Button'),
+
+        MultiFieldPanel([
+            FieldPanel('bottom_heading'),
+            FieldPanel('bottom_image_one'),
+            FieldPanel('bottom_image_two'),
+            FieldPanel('bottom_button_text'),
+            FieldPanel('bottom_link_page'),
+        ], heading='Add Bottom Section'),
+
 
     ]
 
@@ -121,6 +221,27 @@ class EventPage(RichTextPageAbstract):
         verbose_name_plural = 'Event Pages'
 
 
+    # def update_context(self,context):
+    #     events = EventPage.objects.all()
+    #     context.update({
+    #         'events': events,
+    
+    #     })
+    #     return context
+
+    def serve(self,request,*args, **kwargs):
+        request.is_preview = False
+        template = self.get_template(request, *args, **kwargs)
+        context = self.get_context(request, *args, **kwargs)
+        # context = self.update_context(default_context)
+        return TemplateResponse(
+            request,
+            template,
+            context,
+        )
+
+
+
 
 class  EventPageImages(Orderable):
     page = ParentalKey(
@@ -128,6 +249,7 @@ class  EventPageImages(Orderable):
         on_delete=models.CASCADE,
         related_name='event_page_images',
     )
+    title = models.CharField(max_length=200,null=True,blank=True)
     image = models.ForeignKey(
         'wagtailimages.Image',
         on_delete=models.SET_NULL,
@@ -136,6 +258,7 @@ class  EventPageImages(Orderable):
         null=True,
     )
     panels = [
+        FieldPanel('title'),
         FieldPanel('image'),
     ]
     class Meta:
