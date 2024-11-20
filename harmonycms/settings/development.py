005bbbc28621dab1619d704cb2a4b7f3d8a7b40e
shubhamwadhwa711 <shubhamwadhwa711@gmail.com>
2024-11-20 12:34:01 +0530

    fix db

--- harmonycms/settings/development.py
+++ harmonycms/settings/development.py
@@ -11,23 +11,23 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("DB_NAME"),
-#         "USER": os.getenv("DB_USER"),
-#         "PASSWORD": os.getenv("DB_PASSWORD"),
-#         "HOST": os.getenv("DB_HOST"),
-#         'PORT': os.getenv('DB_PORT', '5432'),
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
+}
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
