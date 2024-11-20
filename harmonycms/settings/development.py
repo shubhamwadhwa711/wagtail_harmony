98f6a389f3256b444de2802c464344cf9f58f558
shubhamwadhwa711 <shubhamwadhwa711@gmail.com>
2024-11-20 14:22:10 +0530

    csrf

--- harmonycms/settings/development.py
+++ harmonycms/settings/development.py
@@ -11,13 +11,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 
 DATABASES = {
     "default": {
@@ -31,3 +24,29 @@ DATABASES = {
 }
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+CSRF_TRUSTED_ORIGINS = [ "https://harmony.shubpy.com","http://localhost:8000","http://harmony.shubpy.com"]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8000",
+#     "http://127.0.0.1:8000",
+#     "https://harmony.shubpy.com",
+# ]
+
+# CORS_ALLOW_HEADERS = [
+#     "accept",
+#     "accept-encoding",
+#     "authorization",
+#     "content-type",
+#     "dnt",
+#     "origin",
+#     "user-agent",
+#     "x-csrftoken",
+#     "x-requested-with",
+#     # "access-control-allow-origin",
+#     "Authorization",
+    
+# ]
+
